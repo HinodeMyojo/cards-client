0d2d3641d4458789e996fabb4b2ccd56ecfb279a
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-10-31 23:26:24 +0300

    fix: изменена дефолтная тема vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,22 +2,8 @@ import 'vuetify/styles'
 import '@/styles/main.scss' 
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-    {
-        theme: {
-            themes: {
-                dark: {
-                    colors: {
-                        primary: '#00b0ff',
-                        secondary: '#424242',
-                        accent: '#82B1FF',
-                        error: '#FF5252',
-                        info: '#2196F3',
-                        success: '#4CAF50',
-                        warning: '#FFC107'
-                    }
-                }
-            }
-        }
-    }
-) 
+export default createVuetify({
+    theme: {
+      defaultTheme: 'dark',
+    },
+  })
