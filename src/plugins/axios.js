d546d4c8d569530728ad97458de401595a00b28c
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2024-12-12 02:28:54 +0300

    feat: добавлена обработка 404

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -43,6 +43,9 @@ api.interceptors.response.use((response) =>
                 router.push('/login')
             }
         }
+        else if (error.response.status === 404) {
+            console.error(error);
+        }
         else{
             console.error(error);
             authStore.cleanData();
