ad66e4e4ea7628f0c14f6ce25372bab111ed48e7
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-16 02:45:34 +0300

    feat: закончена верстка инстанса модулей на главной странице

---
+++ src/components/UI/buttons/LinkButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="main-link">
+        <button class="link-button" @click="copyLinkToClipBoard"><svg-icon type="mdi" :path="path"></svg-icon></button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiLinkVariant } from '@mdi/js';
+
+const path = ref(mdiLinkVariant);
+
+const prefixLink = ref(window.location.origin);
+
+const props = defineProps({
+    link: String
+})
+
+const copyLinkToClipBoard = () => {
+    navigator.clipboard.writeText(`${prefixLink.value}/${props.link}`);
+}
+
+</script>
+
+<style scoped>
+.link-button:active {
+    scale: 0.95;
+    transition: all 0.2s ease;
+}
+</style>
