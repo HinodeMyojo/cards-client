a01c721dd8a4b476d6bd7e33f067a2dd1b15061e
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-18 05:08:28 +0300

    feat: главная страница в процессе

---
+++ src/components/UI/buttons/DropDown copy.vue
@@ -0,0 +1,198 @@
+<!-- <template>
+    <div class="dropdown">
+        <button :class="buttonClass" class="base" :style="buttonStyle" @click="handleClick" :disabled="disabled">
+            <slot>{{ label }}</slot>
+            <svg-icon type="mdi" :path="path"></svg-icon>
+        </button>
+        <div class="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { computed, ref } from 'vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiChevronDown } from '@mdi/js';
+
+const path = ref(mdiChevronDown);
+
+const props = defineProps({
+    label: {
+        type: String,
+        default: 'Click me',
+    },
+    color: {
+        type: String,
+        default: 'primary',
+    },
+    size: {
+        type: String,
+        default: 'small',
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+    width: {
+        type: String,
+        default: '120px',
+    },
+    height: {
+        type: String,
+        default: '30px',
+    },
+    borderColor: {
+        type: String,
+        default: 'none',
+    },
+    borderWidth: {
+        type: String,
+        default: '1px',
+    },
+    hoverColor: {
+        type: String,
+        default: 'transparent',
+    },
+
+});
+
+const emit = defineEmits(['click']);
+
+const buttonClass = computed(() => {
+    return `btn ${props.color} ${props.size}`;
+});
+
+const buttonStyle = computed(() => {
+    return {
+        backgroundColor: props.color === 'primary' ? '#202127' : props.color,
+        width: props.width,
+        height: props.height,
+        borderColor: props.borderColor,
+        borderWidth: props.borderWidth,
+        '--hover-border-color': props.hoverColor,
+    };
+});
+
+const handleClick = (event) => {
+    if (!props.disabled) {
+        emit('click', event);
+    }
+};
+</script>
+
+<style scoped>
+.btn {
+    padding: 10px 20px;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    position: relative;
+    justify-content: space-around;
+    font-size: 16px;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    position: relative;
+    overflow: hidden;
+    border: 1px solid transparent;
+    transition: 0.3s ease;
+}
+
+/* The container <div> - needed to position the dropdown content */
+.dropdown {
+    position: relative;
+    display: inline-block;
+}
+
+/* Dropdown Content (Hidden by Default) */
+.dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+}
+
+/* Links inside the dropdown */
+.dropdown-content a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+/* Change color of dropdown links on hover */
+.dropdown-content a:hover {
+    background-color: #f1f1f1
+}
+
+/* Show the dropdown menu on hover */
+.dropdown:hover .dropdown-content {
+    display: block;
+}
+
+/* Hover Effect */
+.btn:hover {
+    box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+    border: 1px solid var(--hover-border-color);
+    transition: 0.3s ease;
+}
+
+/* Active Effect */
+.btn:active {
+    transform: scale(0.98);
+    background-color: darken(#42b983, 10%);
+}
+
+/* Disabled Style */
+.btn:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+    box-shadow: none;
+}
+
+/* Ripple Effect */
+.btn::after {
+    content: '';
+    position: absolute;
+    border-radius: 50%;
+    transform: scale(0);
+    background-color: rgba(255, 255, 255, 0.5);
+    transition: transform 0.5s, opacity 0.8s;
+}
+
+.btn:active::after {
+    width: 200%;
+    padding-top: 200%;
+    transform: scale(1);
+    opacity: 0;
+    transition: transform 0s;
+}
+
+/* Color Variants */
+.primary {
+    background-color: #42b983;
+    color: white;
+}
+
+.secondary {
+    background-color: #ccc;
+    color: black;
+}
+
+/* Size Variants */
+.medium {
+    font-size: 16px;
+}
+
+.large {
+    font-size: 20px;
+}
+
+.small {
+    font-size: 12px;
+}
+</style> -->
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,12 +1,246 @@
 <template>
-    <div>
-        <!-- Vue content -->
+    <div class="main-homepage">
+        <div v-if="isAuth" class="main-block">
+            <div class="radius">
+                <h3 class="gradient-border-mask-1">Ваша статистика</h3>
+            </div>
+            <div class="homepage-statistic">
+                <div class="left-statistic-block">
+                    <div class="block-items">
+                        <p>Нужно пройти для закрепления:</p>
+                        <div class="items">
+                            <div v-for="item in currentStatisticContainer" :key="item">
+                                <HomepageStatistic :idModule="item.idModule" :time="item.time" :typeTime="item.typeTime"
+                                    :text="item.text" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="block-items">
+                        <p>Успешно пройденные модули:</p>
+                        <div class="items">
+                            <div v-for="item in oldStatisticContainer" :key="item">
+                                <HomepageStatistic :idModule="item.idModule" :time="item.time" :typeTime="item.typeTime"
+                                    :text="item.text" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="right-statistic-block">
+                    <div class="statistic-chart">
+                        <p>График активности за сегодня:</p>
+                        <div class="chart">
+                            <HomepageChart />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-block">
+            <div class="radius">
+                <h3 class="gradient-border-mask-1">Модули других пользователей</h3>
+            </div>
+        </div>
+        <div class="main-block">
+            <div class="radius">
+                <h3 class="gradient-border-mask-1">Новости</h3>
+            </div>
+        </div>
+
+        <!-- <HomepageModule /> -->
+        <!-- <HomepageNews /> -->
+        <!-- <HomepageChart /> -->
+        <!-- <DropDown /> -->
+        <!-- <HomePage /> -->
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import HomepageModule from '@/components/homepageElements/HomepageModule.vue'
+import HomepageStatistic from '@/components/homepageElements/HomepageStatistic.vue'
+import HomepageChart from '@/components/homepageElements/HomepageChart.vue'
+import DropDown from '@/components/UI/buttons/DropDown.vue'
+
+const isAuth = ref(true);
+
+// временно заполним
+const currentStatisticContainer = ref([
+    {
+        idModule: 1,
+        time: "сейчас",
+        typeTime: 1,
+        text: "название модуля"
+    },
+    {
+        idModule: 2,
+        time: "через 3 часа",
+        typeTime: 2,
+        text: "название модуля"
+    },
+    {
+        idModule: 3,
+        time: "Завтра",
+        typeTime: 3,
+        text: "название модуля"
+    }
+]);
+
+const oldStatisticContainer = ref([
+    {
+        idModule: 1,
+        time: "6 минут назад",
+        typeTime: 4,
+        text: "название модуля"
+    },
+    {
+        idModule: 1,
+        time: "2 часа назад",
+        typeTime: 4,
+        text: "апкурикетприваркптами"
+    },
+    {
+        idModule: 3,
+        time: "20 часов назад",
+        typeTime: 4,
+        text: "Бпкаолппоалдпапаппакуап"
+    }
+]);
+
+
+// За два дня (пока последние три)
+// [{
+//  idModule: 1, 
+//  time: "через 3 минуты", 
+//  typeTime: 1, (1 - сейчас (просрочк), 2 - 3 часа, 3 - завтра, 4 - пройдено)
+//  text: "название модуля" 
+// }]
+
+
 </script>
 
 <style scoped>
-/* Add styles here */
+p {
+    font-weight: 500;
+}
+
+.main-homepage {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+.main-block {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+
+.main-homepage-separator {
+    position: relative;
+    border: 1px solid #D9D9D9;
+    border-radius: 20px;
+}
+
+.homepage-statistic {
+    display: flex;
+    flex-direction: row;
+    max-height: 300px;
+}
+
+.left-statistic-block {
+    display: flex;
+    flex-direction: column;
+    flex: 2;
+    gap: 15px;
+}
+
+.right-statistic-block {
+    display: flex;
+    flex: 1;
+}
+
+.block-items {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
+.items {
+    display: flex;
+    flex-direction: row;
+    gap: 15px;
+}
+
+.statistic-chart {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+}
+
+.chart {
+    height: 100%;
+}
+
+.radius {
+    border-radius: 15px;
+    overflow: hidden;
+}
+
+.gradient-border-bg {
+    border: 15px solid;
+    border-image: linear-gradient(45deg, purple, orange) 1;
+    padding: 0.5px;
+}
+
+.gradient-border-mask {
+    display: flow-root;
+    position: relative;
+    padding: 0.5rem;
+}
+
+.gradient-border-mask::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 15px;
+    border: 3px solid transparent;
+    background: linear-gradient(45deg, purple, orange) border-box;
+    -webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+}
+
+.gradient-border-mask-1 {
+    display: flow-root;
+    position: relative;
+    padding: 0.5rem;
+}
+
+.gradient-border-mask-1::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border-radius: 15px;
+    border: 3px solid transparent;
+    background: linear-gradient(45deg, rgb(91, 21, 145), rgba(255, 166, 0, 0)) border-box;
+    -webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+}
+
+
+.main-homepage-separator-text {
+    padding: 10px;
+    font-size: 18px;
+}
 </style>
