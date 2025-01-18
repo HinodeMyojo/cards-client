fc7e9cfa2924f8bb18f0f3a0731c2d215000e25b
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-18 05:07:13 +0300

    feat: реализован график

---
+++ src/components/homepageElements/HomepageChart.vue
@@ -0,0 +1,145 @@
+<template>
+    <div :style="{ width: props.width, height: props.height }">
+        <apexchart :width="'100%'" :height="'100%'" type="donut" :options="options" :series="props.series"
+            :labels="labels" :colors="colors">
+        </apexchart>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+    series: {
+        type: Array,
+        default: () => [1, 1, 1, 1]
+    },
+    width: {
+        type: String,
+        default: '100%'
+    },
+    height: {
+        type: String,
+        default: '100%'
+    }
+})
+
+
+// Chart options and series data
+const options = ref({
+    chart: {
+        id: 'donut-chart',
+        height: '100%'
+    },
+    plotOptions: {
+        pie: {
+            customScale: 1
+        }
+    },
+    redrawOnParentResize: false,
+    labels: ['Более 80%', 'Более 30%', 'Более 0%', 'Есть к чему стремиться!'],
+    colors: ['#00EC93', '#6dea98', '#a1ecbb', '#f1f1f1'],
+    dataLabels: {
+        style: {
+            colors: ['#2b2b2b', '#2b2b2b', '#2b2b2b', '#2b2b2b']
+        }
+    },
+    legend: {
+        show: false,
+        position: 'right',
+        horizontalAlign: 'center',
+        fontSize: '14px',
+        fontFamily: 'Helvetica, Arial',
+        fontWeight: 500,
+        markers: {
+            size: 7,
+            strokeWidth: 1,
+        },
+        itemMargin: {
+            horizontal: 5,
+        },
+        onItemClick: {
+            toggleDataSeries: true
+        },
+    },
+    dataLabels: {
+        enabled: true,
+        enabledOnSeries: undefined,
+        formatter: function (val) {
+            return val
+        },
+        textAnchor: 'middle',
+        distributed: false,
+        offsetX: 0,
+        offsetY: 0,
+        style: {
+            fontSize: '14px',
+            fontFamily: 'Helvetica, Arial, sans-serif',
+            fontWeight: 'bold',
+            colors: undefined
+        },
+        background: {
+            enabled: true,
+            foreColor: '#1B1B1F',
+            padding: 4,
+            borderRadius: 2,
+            borderWidth: 1,
+            borderColor: '#1B1B1F',
+            opacity: 0.9,
+            dropShadow: {
+                enabled: false,
+                top: 1,
+                left: 1,
+                blur: 1,
+                color: '#000',
+                opacity: 0.45
+            }
+        },
+        dropShadow: {
+            enabled: false,
+            top: 1,
+            left: 1,
+            blur: 1,
+            color: '#000',
+            opacity: 0.45
+        }
+    },
+    tooltip: {
+        enabled: true,
+        shared: true,
+        style: {
+            fontSize: '14px',
+            color: '#2b2b2b'
+        },
+        theme: 'dark',
+        x: {
+            show: true,
+            format: 'dd MMM',
+        },
+        y: {
+            title: {
+                formatter: (seriesName) => seriesName,
+            },
+        },
+    },
+    stroke: {
+        show: true,
+        curve: 'monotoneCubic',
+        lineCap: 'square',
+        colors: 'transparent',
+        width: 1,
+        dashArray: 0,
+    }
+});
+
+</script>
+<style>
+.apexcharts-tooltip {
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 14px;
+}
+
+.apexcharts-tooltip span {
+    color: rgb(1, 15, 15);
+}
+</style>
