0e682d38e3988feff3c42c1abe77149c37a2e0b3
Egor Semenov <135758742+HinodeMyojo@users.noreply.github.com>
2025-01-27 23:51:56 +0300

    fix: мелочи

--- src/components/homepageElements/HomepageSlider.vue
+++ src/components/homepageElements/HomepageSlider.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="main">
-    <swiper
-      :spaceBetween="30"
-      :centeredSlides="true"
-      :autoplay="{
-        delay: 4000,
-        disableOnInteraction: true,
-        pauseOnMouseEnter: true,
-      }"
-      :navigation="true"
-      :modules="modules"
-      class="mySwiper"
-    >
+    <swiper :spaceBetween="30" :centeredSlides="true" :autoplay="{
+      delay: 4000,
+      disableOnInteraction: true,
+      pauseOnMouseEnter: true,
+    }" :navigation="true" :modules="modules" class="mySwiper">
       <swiper-slide>
         <div class="image-slider">
           <h3>Легко осваивайте новую информацию с помощью карточек</h3>
@@ -34,45 +27,45 @@
   </div>
 </template>
 <script setup>
-  // Import Swiper Vue.js components
-  import { Swiper, SwiperSlide } from 'swiper/vue';
-  import { ref } from 'vue';
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { ref } from 'vue';
 
-  // Import Swiper styles
-  import 'swiper/css';
+// Import Swiper styles
+import 'swiper/css';
 
-  import 'swiper/css/pagination';
-  import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-  // import required modules
-  import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
-  const modules = ref([Autoplay, Pagination, Navigation]);
+const modules = ref([Autoplay, Pagination, Navigation]);
 </script>
 
 <style scoped>
-  .image-slider {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    width: 100%;
-    height: 400px;
-    gap: 30px;
-  }
+.image-slider {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  width: 100%;
+  height: 400px;
+  gap: 30px;
+}
 
-  .image-slider h3 {
-    width: 400px;
-    font-size: 24px;
-  }
+.image-slider h3 {
+  width: 400px;
+  font-size: 24px;
+}
 
-  .image-slider img {
-    object-fit: cover;
-    /* width: 100%; */
-    height: 100%;
-  }
+.image-slider img {
+  object-fit: cover;
+  /* width: 100%; */
+  height: 100%;
+}
 
-  .tinted-image {
-    filter: brightness(0.8); /* Уменьшает яркость на 20% */
-  }
+.tinted-image {
+  filter: brightness(0.8);
+}
 </style>
